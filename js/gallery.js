a10560fe0a2a3a024b119e0b1fb03e38fe0cc80f
Dave Gumba <dave_gumba@Yahoo.com>
2017-08-08 21:15:21 -0400

    Edited

--- js/gallery.js
+++ js/gallery.js
@@ -1,17 +1,10 @@
 let $photoGallery = $('.photo-gallery');
-let $videoGallery = $('.video-gallery');
 let imageNum = 1;
 let numImages = 17;
-let videoNum = 1;
-let numVideos = 2;
 
 // don't hardcode this somehow
 for (let i = 1; i < numImages + 1; i++) {
   $photoGallery.append(imageCardTemplate(i, `dataCaption${i}`, `cardTitle${i}`, `cardContent${i}`));
-}
-
-for (let j = 1; j < numVideos + 1; j++) {
-  $videoGallery.append(videoTemplate(j));
 }
 
 function imageCardTemplate(ind, dataCaption, cardTitle, cardContent) {
@@ -28,14 +21,4 @@ function imageCardTemplate(ind, dataCaption, cardTitle, cardContent) {
               </div>
             </div>
           </div>`
-}
-
-function videoTemplate(ind) {
-  return `<div class="col s12">
-            <video class="responsive-video" controls>
-              <source src="assets/videos/vid${ind}.mp4" type="video/mp4">
-            </video>
-          </div>
-          <br>
-        `
 }
